3153d88d2f666879aa63c4c1dd12b737f28fce32
Nick <nickmagi24@gmail.com>
2021-09-23 12:06:16 -0400

    Function setup

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-//Book excercise
+let myLibrary = [];
+
 function Books(title, author, pages, read) {
     this.title = title
     this.author = author
@@ -9,13 +10,19 @@ function Books(title, author, pages, read) {
     }
 }
 
-const book1 = new Books('Invincible', 'Robert Kirkman', 400, "I've read it!");
-const book2 = new Books('Astrophysics for People in a Hurry', "Neil de'Grasse Tyson", 244, 'Also read it!');
+function addBookToLibrary() {
 
-console.log(book1.info());
-console.log(book2.info());
+}
 
 
-Books.prototype = Object.create(Books.prototype);
-const code = new Books('Code', 'Petzold', 393, 'Read some of it.');
-console.log(code.info()); 
+
+// const book1 = new Books('Invincible', 'Robert Kirkman', 400, "I've read it!");
+// const book2 = new Books('Astrophysics for People in a Hurry', "Neil de'Grasse Tyson", 244, 'Also read it!');
+
+// console.log(book1.info());
+// console.log(book2.info());
+
+
+// Books.prototype = Object.create(Books.prototype);
+// const code = new Books('Code', 'Petzold', 393, 'Read some of it.');
+// console.log(code.info()); 
