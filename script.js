81519eedb568ccd5e34ffd2282bb745ec4ced8a0
Nick <nickmagi24@gmail.com>
2021-09-23 12:01:58 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+//Book excercise
+function Books(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.info = function() {
+        return (title + ' by ' + author + ', ' + pages + ' pages' + ', ' + read)
+    }
+}
+
+const book1 = new Books('Invincible', 'Robert Kirkman', 400, "I've read it!");
+const book2 = new Books('Astrophysics for People in a Hurry', "Neil de'Grasse Tyson", 244, 'Also read it!');
+
+console.log(book1.info());
+console.log(book2.info());
+
+
+Books.prototype = Object.create(Books.prototype);
+const code = new Books('Code', 'Petzold', 393, 'Read some of it.');
+console.log(code.info()); 
