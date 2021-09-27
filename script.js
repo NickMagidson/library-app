c19bf1e09b30ec7f77923aad71d1e5073074c0cc
Nick <nickmagi24@gmail.com>
2021-09-27 13:05:05 -0400

    Render function and refractor

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
 // Event listener buttons for create new book, add new book, and remove pop up.
-const addBtn = document.querySelector('addBtn');
-addBtn.addEventListener('click', addBookToLibrary());
+const addBtn = document.querySelector('#addBtn');
+addBtn.addEventListener('click', addBookToLibrary);
 
-const newBookBtn = document.querySelector('newBtn');
-newBookBtn.addEventListener('click', popUpForm.style.display = 'block');
+const newBookBtn = document.querySelector('#newBtn');
+newBookBtn.addEventListener('click', () => popUpForm.style.display = 'block');
 
-const popUpForm = document.getElementById('form');
+const popUpForm = document.getElementById('popUp');
 const closePopUp = document.getElementsByTagName('span')[0];
-closePopUp.addEventListener('click', popUpForm.style.display = 'none');
+closePopUp.addEventListener('click', () => popUpForm.style.display = 'none');
 
-
-
-let myLibrary = []; // Add a few book objects to array to test display
-let newBook;
 
 class Book {
     constructor(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
+        this.title = form.title.value; 
+        this.author = form.author.value; 
+        this.pages = form.pages.value + 'pg'; 
+        this.read = form.read.checked; 
     }
 }
+
+let myLibrary = []; // Add a few book objects to array to test display
+let newBook;
 
 // Takes user input and store new book object into array
 function addBookToLibrary() {
@@ -34,9 +33,16 @@ function addBookToLibrary() {
     form.reset();
 }
 
-// Remove book button
-function bookRemoval() {
-
+// Loops through array and displays each book in own card.
+function render() {
+    const display = document.getElementById('Library-container');
+    const books = document.querySelectorAll('.book');
+    books.forEach(book => display.removeChild(book));
+    
+    //for loop to iterate through myLibrary
+    for (let i = 0; i < myLibrary.length; i++) {
+        createBook(myLibrary[i]);
+    }
 }
 
 // Creates DOM element for render()
@@ -95,40 +101,3 @@ function createBook(item) {
 
 };
 
-
-// Loops through array and displays each book in own card.
-function render() {
-    const display = document.getElementById('Library-container');
-    const books = document.querySelectorAll('.book');
-    books.forEach(book => display.removeChild(book));
-    
-    //for loop to iterate through myLibrary
-    for (let i = 0; i < myLibrary; i++) {
-        createBook(myLibrary[i]);
-        
-    }
-}
-
-
-//Book DOM elements for render()
-
-
-
-
-
-
-
-
-
-
-
-// const book1 = new Books('Invincible', 'Robert Kirkman', 400, "I've read it!");
-// const book2 = new Books('Astrophysics for People in a Hurry', "Neil de'Grasse Tyson", 244, 'Also read it!');
-
-// console.log(book1.info());
-// console.log(book2.info());
-
-
-// Books.prototype = Object.create(Books.prototype);
-// const code = new Books('Code', 'Petzold', 393, 'Read some of it.');
-// console.log(code.info()); 
